e3b3a9c08dfa31986df2cac35d2f046f032cc8d2
Rahul Biswas <57568379+go-rahul@users.noreply.github.com>
2021-01-07 16:38:11 +0530

    Added global CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
 import { render } from "react-dom";
-import { App } from "./App";
+import { App } from "./components/App";
+import "./index.css"; // CSS
 
 render(<App />, document.getElementById("root"));
